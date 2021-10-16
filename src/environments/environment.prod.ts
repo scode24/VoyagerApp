77bbec93f71e9ab77615d1359db38ebf034eef7d
scode24 <sarkar.soumyabrata2@gmail.com>
2021-10-16 19:01:35 +0530

    environment prod key entry

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  title: "Voyager",
+  gameConfig: {
+    pixerKey: "563492ad6f917000010000015d18c4579be444bbae240d06b8cd7a0c",
+    noOfAllowedWrongAttempts: 5,
+    scoreIncrementBy: 5
+  }
 };
